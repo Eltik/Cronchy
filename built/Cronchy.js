5a26f98677a022abf6c2524846cce1091e47827a
Eltik <xp10d363@gmail.com>
2023-01-06 16:36:34 -0800

    Removed axios and now using node-fetch.

--- built/Cronchy.js
+++ built/Cronchy.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = require("axios");
+const promise_request_1 = require("./libraries/promise-request");
 class Cronchy {
     /**
      * @param email The email of the account.
@@ -20,43 +20,55 @@ class Cronchy {
      * @important Must be run before any other function.
     */
     async login() {
-        const response = await axios_1.default.post(`${this.api}/auth/v1/token`, {
-            username: this.email,
-            password: this.password,
-            grant_type: "password",
-            scope: "offline_access"
-        }, {
+        const params = new URLSearchParams();
+        params.append("username", this.email);
+        params.append("password", this.password);
+        params.append("grant_type", "password");
+        params.append("scope", "offline_access");
+        const req = new promise_request_1.default(`${this.api}/auth/v1/token`, {
+            body: params,
             headers: {
                 Authorization: `Basic ${this.token}`,
                 "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }).catch((err) => {
+            },
+            method: "POST"
+        });
+        const res = await req.request().catch((err) => {
             throw new Error(`Request to ${this.api}/auth/v1/token failed.\nEmail: ${this.email}; Password: ${this.password}; Token: ${this.token}`);
         });
+        if (res.status != 200) {
+            throw new Error(res.toString());
+        }
+        const response = res.json();
         const data = {
-            access_token: response.data["access_token"],
-            refresh_token: response.data["refresh_token"],
-            expires_in: response.data["expires_in"],
-            token_type: response.data["token_type"],
-            scope: response.data["scope"],
-            country: response.data["country"],
-            account_id: response.data["account_id"]
+            access_token: response["access_token"],
+            refresh_token: response["refresh_token"],
+            expires_in: response["expires_in"],
+            token_type: response["token_type"],
+            scope: response["scope"],
+            country: response["country"],
+            account_id: response["account_id"]
         };
         this.accessToken = data.access_token;
         this.refreshToken = data.refresh_token;
         this.accountId = data.account_id;
-        const signature = await axios_1.default.get(`${this.api}/index/v2`, {
+        const sigReq = new promise_request_1.default(`${this.api}/index/v2`, {
             headers: {
                 Authorization: "Bearer " + data.access_token,
             },
-        }).catch((err) => {
+        });
+        const sig = await sigReq.request().catch((err) => {
             throw new Error(`Request to ${this.api}/index/v2 failed.\nBearer: ${data.access_token}`);
         });
+        const signature = sig.json();
+        if (!signature || !signature.cms) {
+            throw new Error(`Request to ${this.api}/index/v2 failed.\nBearer: ${data.access_token}`);
+        }
         const sig_data = {
-            signature: signature.data ? signature.data.cms.signature : "",
-            key_pair_id: signature.data ? signature.data.cms.key_pair_id : "",
-            bucket: signature.data ? signature.data.cms.bucket : "",
-            policy: signature.data ? signature.data.cms.policy : "",
+            signature: signature.cms.signature,
+            key_pair_id: signature.cms.key_pair_id,
+            bucket: signature.cms.bucket,
+            policy: signature.cms.policy,
         };
         this.signature = sig_data.signature;
         this.key_pair_id = sig_data.key_pair_id;
@@ -80,75 +92,91 @@ class Cronchy {
      * @param amount Max amount of search results. Takes a number.
     */
     async search(query, amount) {
-        const request = await axios_1.default.get(`${this.api}/content/v2/discover/search?q=${encodeURIComponent(query)}&n=${amount}&type=&locale=en-US`, {
+        const req = new promise_request_1.default(`${this.api}/content/v2/discover/search?q=${encodeURIComponent(query)}&n=${amount}&type=&locale=en-US`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
                 Referer: this.main
-            }
-        }).catch((err) => {
+            },
+        });
+        const search = await req.request().catch((err) => {
             throw new Error(`Request to ${this.api}/content/v2/discover/search?q=${encodeURIComponent(query)}&n=${amount}&type=&locale=en-US failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
         });
-        return request.data;
+        if (search.status != 200) {
+            throw new Error(search.toString());
+        }
+        return search.json();
     }
     async queryShowData(id, locale, mediaType) {
-        const cr_data = await axios_1.default.get(`${this.api}/content/v2/cms/${mediaType}/${id}?locale=${locale}`, {
+        const req = new promise_request_1.default(`${this.api}/content/v2/cms/${mediaType}/${id}?locale=${locale}`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
                 Referer: this.main
-            }
-        }).catch((err) => {
+            },
+        });
+        const cr_data = await req.request().catch((err) => {
             throw new Error(`Request to ${this.api}/content/v2/cms/${mediaType}/${id}?locale=${locale} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
         });
-        return cr_data["data"];
+        return cr_data.json();
     }
     async queryGenreData(id, locale) {
-        const cr_genre_response = await axios_1.default.get(`${this.api}/content/v1/tenant_categories?guid=${id}?locale=${locale}`, {
+        const req = new promise_request_1.default(`${this.api}/content/v1/tenant_categories?guid=${id}?locale=${locale}`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
+                Referer: this.main
             },
         });
-        return cr_genre_response["data"];
+        const cr_genre_response = await req.request().catch((err) => {
+            throw new Error(`Request to ${this.api}/content/v1/tenant_categories?guid=${id}?locale=${locale} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
+        });
+        return cr_genre_response.json();
     }
     async queryRatings(id) {
-        const cr_ratings_response = await axios_1.default.get(`${this.api}/content-reviews/v2/user/${this.accountId}/rating/series/${id}`, {
+        const req = new promise_request_1.default(`${this.api}/content-reviews/v2/user/${this.accountId}/rating/series/${id}`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
+                Referer: this.main
             },
-        }).catch((err) => {
-            throw new Error(`Request to ${this.api}/content-reviews/v2/user/${this.accountId}/rating/series/${id} failed.\nBearer: ${this.accessToken}`);
         });
-        return cr_ratings_response["data"];
+        const cr_ratings_response = await req.request().catch((err) => {
+            throw new Error(`Request to ${this.api}/content-reviews/v2/user/${this.accountId}/rating/series/${id} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
+        });
+        return cr_ratings_response.json();
     }
     async queryRecommendations(id, locale) {
-        const cr_recommendations_response = await axios_1.default.get(`${this.api}/content/v1/${this.accountId}/similar_to?guid=${id}&locale=${locale}&n=30`, {
+        const req = new promise_request_1.default(`${this.api}/content/v1/${this.accountId}/similar_to?guid=${id}&locale=${locale}&n=30`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
+                Referer: this.main
             },
-        }).catch((err) => {
-            throw new Error(`Request to ${this.api}/content/v1/${this.accountId}/similar_to?guid=${id}&locale=${locale}&n=30 failed.\nBearer: ${this.accessToken}`);
         });
-        return cr_recommendations_response["data"];
+        const cr_recommendations_response = await req.request().catch((err) => {
+            throw new Error(`Request to ${this.api}/content/v1/${this.accountId}/similar_to?guid=${id}&locale=${locale}&n=30 failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
+        });
+        return cr_recommendations_response.json();
     }
     async querySeason(id, locale) {
-        const season_response = await axios_1.default.get(`${this.api}/cms/v2${this.bucket}/seasons?series_id=${id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id}`, {
-            headers: {
-                Authorization: "Bearer " + this.accessToken,
-            },
-        }).catch((err) => {
-            throw new Error(`Request to ${this.api}/cms/v2${this.bucket}/seasons?series_id=${id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id} failed.\nBearer: ${this.accessToken}`);
-        });
-        return season_response["data"];
-    }
-    async queryEpisodes(season, locale) {
-        const episode_response = await axios_1.default.get(`${this.api}/cms/v2${this.bucket}/episodes?season_id=${season.id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id}`, {
+        const req = new promise_request_1.default(`${this.api}/cms/v2${this.bucket}/seasons?series_id=${id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id}`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
-                Referer: this.main,
+                Referer: this.main
             },
-        }).catch((err) => {
+        });
+        const seasons_response = await req.request().catch((err) => {
+            throw new Error(`Request to ${this.api}/cms/v2${this.bucket}/seasons?series_id=${id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
+        });
+        return seasons_response.json();
+    }
+    async queryEpisodes(season, locale) {
+        const req = new promise_request_1.default(`${this.api}/cms/v2${this.bucket}/episodes?season_id=${season.id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id}`, {
+            headers: {
+                Authorization: `Bearer ${this.accessToken}`,
+                Referer: this.main
+            },
+        });
+        const episode_response = await req.request().catch((err) => {
             throw new Error(`Request to ${this.api}/cms/v2${this.bucket}/episodes?season_id=${season.id}&locale=${locale}&Signature=${this.signature}&Policy=${this.policy}&Key-Pair-Id=${this.key_pair_id} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
         });
-        return episode_response["data"];
+        return episode_response.json();
     }
     /**
      * @param seriesQuery SearchQuery object. Can be obtained from the search function.
@@ -264,34 +292,37 @@ class Cronchy {
      * @param locale The locale of the episode. For example, "en-US".
     */
     async getSources(episodeId, locale) {
-        const temp_response = await axios_1.default.get(`${this.api}/content/v2/cms/objects/${episodeId}?locale=${locale}`, {
+        const req = new promise_request_1.default(`${this.api}/content/v2/cms/objects/${episodeId}?locale=${locale}`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
-                Referer: this.main,
+                Referer: this.main
             },
-        }).catch((err) => {
+        });
+        const temp_response = await req.request().catch((err) => {
             throw new Error(`Request to ${this.api}/content/v2/cms/objects/${episodeId}?locale=${locale} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
         });
-        const temp_data = await temp_response.data.data[0];
-        const episode_response = await axios_1.default.get(`${this.api}${temp_data.streams_link}?locale=${locale}`, {
+        const temp_data = temp_response.json().data[0];
+        const episode_req = new promise_request_1.default(`${this.api}${temp_data.streams_link}?locale=${locale}`, {
             headers: {
                 Authorization: `Bearer ${this.accessToken}`,
-                Referer: this.main,
+                Referer: this.main
             },
-        }).catch((err) => {
+        });
+        const episode_response = await episode_req.request().catch((err) => {
             throw new Error(`Request to ${this.api}${temp_data.streams_link}?locale=${locale} failed.\nBearer: ${this.accessToken}; Referer: ${this.main}`);
         });
-        const episode_data = await episode_response.data;
+        const episode_data = episode_response.json();
         const sources = [];
-        const m3u8Urls = await axios_1.default.get(episode_data.data[0].vo_adaptive_hls[""].url, {
+        const m3u8_req = new promise_request_1.default(episode_data.data[0].vo_adaptive_hls[""].url, {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
                 Referer: this.main,
             },
-        }).catch((err) => {
+        });
+        const m3u8Urls = await m3u8_req.request().catch((err) => {
             throw new Error(`Request to ${episode_data.data[0].vo_adaptive_hls[""].url}; User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"; Referer: ${this.main}`);
         });
-        const videoList = m3u8Urls.data.split("#EXT-X-STREAM-INF:");
+        const videoList = m3u8Urls.text().split("#EXT-X-STREAM-INF:");
         for (const video of videoList ?? []) {
             if (!video.includes("m3u8"))
                 continue;
